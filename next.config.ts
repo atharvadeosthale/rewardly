47f6b093fa7cd3a8d2f2b79dd6b73870bc55d560
Atharva Deosthale <atharva.deosthale17@gmail.com>
2025-01-07 00:20:02 +0530

    chore: enable dynamicIO and ppr. also install dotenv for drizzle commands

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Enable dynamic caching and partial prerendering
+  experimental: {
+    dynamicIO: true,
+    ppr: true,
+  },
 };
 
 export default nextConfig;
